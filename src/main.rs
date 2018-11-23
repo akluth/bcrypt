449c74f2d347aeb875fbc59b35523390def4c7af
Alexander Kluth <deralex@cpan.org>
2018-11-23 23:18:29 +0100

    Added --cost/-c option
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 extern crate bcrypt;
 
 use clap::{Arg, App};
-use bcrypt::{DEFAULT_COST, hash, verify};
+use bcrypt::{DEFAULT_COST, hash};
 
 fn main() {
     let matches = App::new("bcrypt")
@@ -13,9 +13,22 @@ fn main() {
                                .help("String to hash")
                                .required(true)
                                .index(1))
+                          .arg(Arg::with_name("cost")
+                               .short("c")
+                               .long("cost")
+                               .value_name("VALUE")
+                               .help("Sets the cost to use (between 4 and 31, defaults to 12)")
+                               .takes_value(true))
                           .get_matches();
 
-    match hash(matches.value_of("STRING").unwrap(), DEFAULT_COST) {
+    let cost;
+
+    match matches.value_of("cost") {
+        Some(c) => cost = c.parse::<u32>().unwrap(),
+        None => cost = DEFAULT_COST
+    }
+
+    match hash(matches.value_of("STRING").unwrap(), cost) {
         Ok(hashed) => println!("{}", hashed),
         Err(e) => println!("{}", e)
     }
